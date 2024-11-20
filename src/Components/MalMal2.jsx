5c6c9719c2a592342fadd1c9823a5c70f2bca4c6
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-11-21 00:29:31 +0530

    deleted mal mal componets

--- src/Components/MalMal2.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function MalMal2() {
-  return <div>MalMal2</div>;
-}
--- src/Components/Malmal.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Malmal() {
-  return <div>Malmal</div>;
-}

f22244f1556b77e2841d9c5ad6206ca5e0d17fa5
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-11-23 17:38:27 +0530

    what we offer compone

--- src/Components/WhatWeOffer.jsx
+++ src/Components/WhatWeOffer.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function WhatWeOffer() {
   return (
-    <div className="w-screen h-screen bg-slate-500">
+    <div className="w-screen h-screen bg-slate-500 relative">
       <div>
+        <div className="bg-slate-950 rounded-full w-3/4 h-auto m-auto items-center absolute "></div>
         <h1>What we Offer</h1>
         <p>At our salon we don't just style hair, we craft confidence</p>
       </div>

ff911878ad7bba6a63c4a1d2da72b10457bcdd87
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-11-23 16:10:57 +0530

    create what we offer

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import MapComponent from "./Components/MapComponent";
 import VortextComponent from "./Components/VortextComponent";
+import WhatWeOffer from "./Components/WhatWeOffer";
 
 function App() {
   return (
     <>
+      <WhatWeOffer />
       <MapComponent />
       <VortextComponent />
       <Footer />
---
+++ src/Components/WhatWeOffer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function WhatWeOffer() {
+  return (
+    <div className="w-screen h-screen bg-slate-500">
+      <div>
+        <h1>What we Offer</h1>
+        <p>At our salon we don't just style hair, we craft confidence</p>
+      </div>
+    </div>
+  );
+}

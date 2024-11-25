e727816e657a848898e97e95b21878f9a429ae1c
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-11-25 22:40:28 +0530

    Change the map

--- src/Components/MapComponent.jsx
+++ src/Components/MapComponent.jsx
@@ -30,33 +30,16 @@
 // export default MapComponent;
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet"; // Import Leaflet for custom marker configuration
-import "leaflet/dist/leaflet.css"; // Import default Leaflet styles
-
-// Import default Leaflet marker images
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-import markerRetina from "leaflet/dist/images/marker-icon-2x.png";
-
-// Fix the default marker icon paths
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerRetina,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
-});
+import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
   const position = [25.790654, -80.130045]; // Latitude and Longitude for South Beach, Miami
 
   return (
-    <div className="relative w-full h-[300px] md:h-[500px] bg-black flex flex-col items-center justify-center rounded-lg overflow-hidden">
+    <div className="relative w-full h-[300px] md:h-[500px] bg-black flex flex-col items-center justify-center rounded-lg overflow-hidden mix-blend-luminosity">
       {/* Title above the map */}
-      <div className="absolute inset-0 flex items-center justify-center z-10">
-        <h2 className="text-secondary-100 text-4xl md:text-6xl mt-32 font-serif">
-          Find Us
-        </h2>
+      <div className="absolute top-4 text-center z-10">
+        <h2 className="text-white text-xl md:text-3xl font-serif">Find Us</h2>
       </div>
       {/* Leaflet Map */}
       <MapContainer
@@ -65,8 +48,8 @@ const MapComponent = () => {
         style={{ width: "100%", height: "100%", zIndex: 1 }}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         <Marker position={position}>
           <Popup>South Beach, Miami, 33139</Popup>

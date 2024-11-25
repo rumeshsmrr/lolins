081570df4bc920a53589983894e56d10064d82df
Pasinduhansana <107157284+Pasinduhansana@users.noreply.github.com>
2024-11-26 00:11:37 +0530

    add file via Upload
    Final Update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,4 @@
+// import React from "react";
 import { FaFacebookF, FaInstagram, FaLinkedin, FaTiktok } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { motion } from "framer-motion";
@@ -20,19 +21,19 @@ const Footer = () => {
       <motion.div
         className="container mx-auto flex flex-col lg:flex-row items-center justify-center lg:justify-between lg:items-start"
         initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
       >
         {/* Left Section */}
         <motion.div
-          className="mb-8 lg:mb-0 w-full md:w-1/2 flex flex-col md:flex-row items-center text-primaryText lg:items-center gap-3"
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
+          className="mb-8 lg:mb-0 w-full md:w-1/2 flex flex-col md:flex-row items-center  text-primaryText lg:items-center gap-3"
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
         >
+          {/* <div className="text-3xl font-primaryFont text-textPrimary011 mb-4  text-primaryText  text-center md:text-start"> */}
           <img src={logo} className="w-28 md:w-40" />
+          {/* </div> */}
           <p className="mb-4 w-full lg:w-3/5 text-center lg:text-start">
             Lolins Hair Salon is a chic, expert space where your hair is
             transformed with personalized care and style.
@@ -43,26 +44,42 @@ const Footer = () => {
           className="w-full md:w-1/2 mb-8 md:mb-0"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
           transition={{ duration: 1 }}
         >
-          <ul className="flex w-full md:pl-7 text-primaryText text-[1.3rem] text-nowrap justify-center md:justify-between flex-row space-y-0 space-x-4 md:space-x-8">
-            {Object.values(links).map((item, index) => (
-              <motion.li
-                key={index}
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-              >
-                <a href={item.link}>{item.name}</a>
-              </motion.li>
-            ))}
+          <ul className="flex w-full md:pl-7  text-primaryText text-[1.3rem]  text-nowrap justify-center md:justify-between flex-row space-y-0 space-x-4 md:space-x-8  ">
+            <motion.nav
+              className="w-full  mb-8 md:mb-0"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <ul className="flex w-full md:pl-7 text-primaryText text-[1.3rem] text-nowrap justify-center md:justify-between flex-row space-y-0 space-x-4 md:space-x-8 ">
+                {Object.values(links).map((item, index) => (
+                  <motion.li
+                    key={index}
+                    id="link"
+                    className=""
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                  >
+                    <a href={item.link}>{item.name}</a>
+                  </motion.li>
+                ))}
+              </ul>
+            </motion.nav>
           </ul>
           <div className="w-full md:pl-7 mt-10 text-primaryText flex justify-between">
-            <div>
+            <div className="mb-0">
               <h2 className="text-textPrimary011 font-stylishBold text-[1.3rem] mb-4">
                 Opening Hours
               </h2>
+              {/* <p className="text-textSecondary011">+1 (999) 888-77-66</p>
+              <p className="text-textSecondary011">hello@kayujati.com</p> */}
               <ul>
+                <li className="flex w-36 justify-between">
+                  <div>MON: </div>
+                  <div>9am - 10pm</div>
+                </li>
                 <li className="flex w-36 justify-between">
                   <div>MON: </div>
                   <div>9am - 10pm</div>
@@ -85,7 +102,7 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div className="text-right">
+            <div className="mb-0 text-right">
               <h2 className="text-textPrimary011 font-stylishBold text-[1.3rem] mb-4">
                 <a href="#contactUs"> Contact Us</a>
               </h2>
@@ -97,17 +114,15 @@ const Footer = () => {
       </motion.div>
 
       <motion.div
-        className="container mx-auto text-primaryText flex flex-col-reverse md:flex-row justify-between gap-16 items-end mt-10"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
+        className="container mx-auto  text-primaryText  flex flex-col-reverse md:flex-row justify-between gap-16 items-end mt-10"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
       >
         <motion.div
           className="w-full md:w-1/2 flex justify-between items-end"
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
         >
           <div className="flex flex-row space-y-4 items-end gap-4">
@@ -119,7 +134,7 @@ const Footer = () => {
               ].map((button, index) => (
                 <motion.button
                   key={index}
-                  className="w-10 h-10 rounded-full bg-slate-200 text-black flex items-center justify-center"
+                  className="w-10 h-10 rounded-full bg-slate-200 text-black  flex items-center justify-center"
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.95 }}
                   aria-label={button.label}
@@ -145,6 +160,7 @@ const Footer = () => {
               ))}
             </div>
           </div>
+
           <p className="text-sm text-textSecondary011 mt-6">
             © 2024 — Copyright <br /> All Rights reserved
           </p>
@@ -152,17 +168,16 @@ const Footer = () => {
 
         <motion.div
           className="w-full md:w-1/2 h-full md:space-y-8"
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
+          initial={{ opacity: 0, x: 50 }}
+          whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
         >
           <div className="w-full flex justify-between">
-            <div>
+            <div className="m-0">
               <h2 className="text-textPrimary011 font-stylishBold text-[1.3rem] mb-4">
                 Location
               </h2>
-              <p>
+              <p className="">
                 202 Beauty Block, <br />
                 Corner of 5th Main, <br />
                 South Beach, Miami, 33139

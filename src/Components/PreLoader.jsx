390aab8e2bbdb7cbd4f88ff4d4d453e15a25fe89
SadiniWanniarachchi <sadiiw17@gmail.com>
2024-12-26 16:19:17 +0530

    mobile responsive

--- src/Components/PreLoader.jsx
+++ src/Components/PreLoader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png"; // Adjust path to your logo image
+import logo from "../assets/logo.png"; 
 
 const Preloader = () => {
   return (

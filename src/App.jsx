9791d14514fbccc7d8fa506ea9871183d9916d9d
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-12-30 10:27:25 +0530

    redesign/change pre loader image

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate a loading delay 
+    // Simulate a loading delay
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3000); // Adjust time as needed
+    }, 3500); // Adjust time as needed
 
     return () => clearTimeout(timer);
   }, []);
